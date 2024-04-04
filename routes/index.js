733f372f4ed2a55e591eea24f87eb7cc907a6ec3
Leng Tan <letng27@gmail.com>
2024-04-04 19:07:29 -0400

    Add displaying the game's information in the game details page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var sqlite3 = require('sqlite3').verbose(); //verbose provides more detailed stack trace
 var db = new sqlite3.Database('data/projectDB');
+const url = require('url')
 
 exports.index = function (request, response){
 	// index.html
@@ -13,6 +14,18 @@ exports.index = function (request, response){
 
 	response.render('index', {alert: message});
 }
+
+function parseURL(request, response) {
+    const PARSE_QUERY = true //parseQueryStringIfTrue
+    const SLASH_HOST = true //slashDenoteHostIfTrue
+    let urlObj = url.parse(request.url, PARSE_QUERY, SLASH_HOST)
+    console.log('path:')
+    console.log(urlObj.path)
+    console.log('query:')
+    console.log(urlObj.query)
+    //for(x in urlObj.query) console.log(x + ': ' + urlObj.query[x])
+    return urlObj
+  }
 
 exports.games = function(request, response){
     console.log("listing games in the collection")
@@ -28,6 +41,26 @@ exports.games = function(request, response){
 }
 
 exports.gameDetails = function(request, response){
-    let gameId = url.parse(request.url)
+    //extract the gameid from the url
+    let urlObj = parseURL(request, response)
+    console.log(urlObj)
+    let gameId = urlObj.path
+    gameId = gameId.substring(gameId.lastIndexOf("/") + 1, gameId.length)
+    console.log(gameId)
+
+    //retrieve game data from the database
+    let sql = `SELECT * FROM games WHERE gameid = ${gameId}`
     
+    db.all(sql, function(err, rows){
+        // response.render('gameDetails',{
+        //     cover:rows.coverart,
+        //     title:rows.title,
+        //     genre:rows.genre
+        // })
+        response.render('gameDetails', {
+            cover:`<img src = '${rows[0].coverart}' alt = '${rows[0].title}cover'>`,
+            title:rows[0].title,
+            genre:rows[0].genre
+        })
+    })
 }
