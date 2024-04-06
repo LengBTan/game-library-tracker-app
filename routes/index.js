6e56437e09d26277905cdb0e9692970a5f7986d6
Leng <letng27@gmail.com>
2024-04-06 04:07:31 -0400

    refactor and add routes for the views

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,70 @@ var sqlite3 = require('sqlite3').verbose(); //verbose provides more detailed sta
 var db = new sqlite3.Database('data/projectDB');
 const url = require('url')
 
+exports.login = function(request, response){
+	const {username, password} = request.body;
+
+	let authorized = false;
+    let userRole;//store the user's role in the session
+
+	db.all("SELECT * FROM users", function(err, rows){
+		for(var i=0; i<rows.length; i++){
+		    if(rows[i].userid == username && rows[i].password == password){
+				authorized = true;
+                userRole = rows[i].admin;//boolean if user is admin
+			} 
+		}
+		if(authorized){
+			console.log("establishing a session")
+			request.session.userId = username
+            request.session.userRole = userRole
+			request.session.save()
+			response.redirect('/dashboard')
+		}
+		else{
+			response.redirect('/index.html/?status=credfail')
+		}
+	})
+}
+
+exports.register = function(request,response){
+    const {username, password} = request.body;
+    // console.log(username)
+    // console.log(password)
+
+    let sql = `SELECT 1 from USERS where userid = '${username}';`
+
+    db.all(sql, function(err,rows){
+        if(rows.length){//userid already exists
+            response.redirect('/index.html/?status=taken')
+        }
+        else{
+            sql = `INSERT INTO users VALUES ('${username}', '${password}', 0);`
+            db.run(sql)
+            request.session.userId = username
+            request.session.userRole = 0
+			request.session.save()
+			response.redirect('/dashboard')
+        }
+    })
+
+    
+}
+
 exports.index = function (request, response){
 	// index.html
     let message = ''
     if(request.query.status === 'fail'){
-        message = 'authentication failed'
+        message = 'Authentication failed'
+    }
+    else if(request.query.status === 'credfail'){
+        message = 'Incorrect username or password'
     }
     else if(request.query.status === 'logout'){
-        message = 'logged out'
+        message = 'Logged out'
+    }
+    else if(request.query.status === 'taken'){
+        message = 'Username is taken'
     }
 
 	response.render('index', {alert: message});
@@ -19,48 +75,171 @@ function parseURL(request, response) {
     const PARSE_QUERY = true //parseQueryStringIfTrue
     const SLASH_HOST = true //slashDenoteHostIfTrue
     let urlObj = url.parse(request.url, PARSE_QUERY, SLASH_HOST)
-    console.log('path:')
-    console.log(urlObj.path)
-    console.log('query:')
-    console.log(urlObj.query)
-    //for(x in urlObj.query) console.log(x + ': ' + urlObj.query[x])
     return urlObj
-  }
+}
 
-exports.games = function(request, response){
+exports.dashboard = function(request, response){
     console.log("listing games in the collection")
 
-    let sql = `select user_game_list.userid, user_game_list.gameid, games.title 
-            from user_game_list join games on user_game_list.gameid = 
-            games.gameid where userid like '${request.session.userId}'`
-    console.log(sql)
+    let userPageLink = ''
+    if(request.session.userRole){
+        userPageLink = '<a href="/users">Users in the DB</a>'
+    }
+    let sql = `select user_game_list.userid, user_game_list.gameid, games.title,
+            games.coverart from user_game_list join 
+            games on user_game_list.gameid = games.gameid where userid like 
+            '${request.session.userId}';`
     db.all(sql , function(err, rows){
-        response.render('games', {gameEntry:rows})
+        response.render('dashboard', {gameEntry:rows, users:userPageLink})
     })
-    
 }
+
+
+//client id and access token required to use the IGDB API
+//https://api-docs.igdb.com/?javascript#authentication
+//remember to change this if making the project public
+const CLIENT_ID = "pkr8ol36ywojvfk0q6pj9rbgcijmfp"
+const AUTH = "nnnm818tc77yk6arme7l8z2hi76r0p"
+//url of the api
+const apiPath = "https://api.igdb.com/v4/games"
+//set the header for the request, required by the api
+let headers = new Headers()
+headers.append("Client-ID", CLIENT_ID)
+headers.append("Authorization", `Bearer ${AUTH}`);
 
 exports.gameDetails = function(request, response){
     //extract the gameid from the url
     let urlObj = parseURL(request, response)
-    console.log(urlObj)
+    // console.log(urlObj)
     let gameId = urlObj.path
     gameId = gameId.substring(gameId.lastIndexOf("/") + 1, gameId.length)
-    console.log(gameId)
-
-    //retrieve game data from the database
-    let sql = `SELECT * FROM games WHERE gameid = ${gameId}`
     
-    db.all(sql, function(err, rows){
-        // response.render('gameDetails',{
-        //     cover:rows.coverart,
-        //     title:rows.title,
-        //     genre:rows.genre
-        // })
-        response.render('gameDetails', {
-            cover:`<img src = '${rows[0].coverart}' alt = '${rows[0].title}cover'>`,
-            title:rows[0].title,
-            genre:rows[0].genre
+    //https://api-docs.igdb.com/?javascript#game
+	//body for the api request
+	let body = `fields name, cover.*, genres.name, involved_companies.company.name, first_release_date; where id = ${gameId};`
+
+	const options = {
+		"method": "POST",
+		"headers":headers,
+		"body":body
+	}
+
+    //send a post request to the api
+	fetch(apiPath, options)
+	.then((response) => response.json())//recieve a response from the api
+	.then((data) => {//recieve the json data from the api
+		// console.log(data)
+        data.forEach((game)=>{
+            //change url to use larger image from the api
+            let imgurl = game.cover.url.replace("t_thumb","t_720p")
+            // console.log(imgurl)
+            game.cover.url=imgurl
+
+            //change genres to be a single string
+            let genres = ''
+            game.genres.forEach((genre)=>{
+                genres+= genre.name + ", "
+            })
+            genres = genres.slice(0,-2)
+            game.genres = genres
         })
+        response.render('gameDetails', {game: data[0]})
+	})
+	.catch((error) => {
+		console.error(error)
+	})
+
+    
+}
+
+
+exports.searchGame = function(request, response){
+    let game = (request.query.title)//.split(' ').join('+')
+	if(!game) {//dont search an empty string
+	  response.render('searchGame')
+	  return
+	}
+
+	//https://api-docs.igdb.com/?javascript#game
+	//body for the api request
+	let body = `fields name, cover.*; search "${game}"; limit 20;`
+
+	const options = {
+		"method": "POST",
+		"headers":headers,
+		"body":body
+	}
+
+    //send a post request to the api
+	fetch(apiPath, options)
+	.then((response) => response.json())//recieve a response from the api
+	.then((data) => {//recieve the json data from the api
+		// console.log(data)
+        data.forEach((game)=>{
+            //change url to use larger image from the api
+            //console.log(game)
+            if(game.hasOwnProperty('cover')){
+                let imgurl = game.cover.url.replace("t_thumb","t_720p")
+                game.cover.url=imgurl
+            }
+            else{
+                console.log(`Game ${game.id} has no cover art!`)
+            }
+        })
+        response.render('searchGame', {searchResult:data});
+	})
+	.catch((error) => {
+		console.error(error)
+	})
+}
+
+exports.addGame = function(request, response){
+    let gameId = (request.query.id)
+	
+	//https://api-docs.igdb.com/?javascript#game
+	//body for the api request
+	let body = `fields name, cover.*; where id = ${gameId};`
+
+	const options = {
+		"method": "POST",
+		"headers":headers,
+		"body":body
+	}
+
+    //send a post request to the api
+	fetch(apiPath, options)
+	.then((response) => response.json())//recieve a response from the api
+	.then((data) => {//recieve the json data from the api
+        let imgurl = data[0].cover.url.replace("t_thumb","t_720p")
+
+        let sql = `INSERT OR REPLACE INTO games VALUES (${data[0].id}, '${data[0].name}', '${imgurl}');`
+        db.run(sql)
+
+        sql = `INSERT OR REPLACE INTO user_game_list VALUES ('${request.session.userId}',${data[0].id});`
+        db.run(sql)
+        response.redirect('/dashboard')
+	})
+	.catch((error) => {
+		console.error(error)
+	})
+}
+
+exports.deleteGame = function(request, response){
+    let gameId = (request.query.id)
+    console.log("deleting game with id: " + gameId)
+
+    sql = `DELETE FROM user_game_list WHERE userid = '${request.session.userId}' AND gameid = ${gameId};`
+    db.run(sql)
+
+    //delete the game from the games table if it is not being referenced by any other users in user_game_list
+    sql = `DELETE FROM games WHERE NOT EXISTS (SELECT 1 FROM user_game_list WHERE user_game_list.gameid = games.gameid);`
+    db.run(sql)
+    response.redirect('/dashboard')
+}
+
+exports.users = function(request, response){
+    let sql = `SELECT userid, admin FROM users;`
+    db.all(sql, function(err, rows){
+        response.render('users', {users:rows})
     })
 }
--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 var express = require('express')
 var path = require('path')
 const session = require('express-session')
-var sqlite3 = require('sqlite3').verbose(); //verbose provides more detailed stack trace
-var db = new sqlite3.Database('data/projectDB');
 
 var app = express()
 
 const PORT = process.env.PORT || 3000
-
-//client id and access token required to use the IGDB API
-//https://api-docs.igdb.com/?javascript#authentication
-//remember to change this if making the project public
-const CLIENT_ID = "pkr8ol36ywojvfk0q6pj9rbgcijmfp"
-const AUTH = "nnnm818tc77yk6arme7l8z2hi76r0p"
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -21,99 +13,57 @@ app.locals.pretty = true; //to generate pretty view-source code in browser
 
 //read routes modules
 var routes = require('./routes/index');
-const { nextTick } = require('process');
-
 
 //middleware
 
 app.use(express.json())//used to parse html post form
 app.use(express.urlencoded({extended: true}))//used to parse html post form
+app.use(express.static(path.join(__dirname, '/public')));//for js scripts and css
+
+
+const checkAuth = function(request, response, next){//middleware used to check if the user has an established session
+    if (request.session.userId) {
+        next(); //user has a session established, continue with next middleware
+    }
+	else {
+        response.redirect('/index.html/?status=fail');//redirect user to the login page
+    }
+}
+
+const checkRole = function(request, response, next){//middleware used to check if the user is an admin
+	if(request.session.userRole){
+		next();
+	}
+	else{
+		response.redirect('/dashboard')//redirect user to the dashboard
+	}
+}
 
 app.use(session({
 	secret: 'secret',
 	resave: false,
 	saveUninitialized: true,
+	cookie:{maxAge:3600000}//1 hour in milliseconds
 }))
 
 //routes
-app.post('/login', (request, response) =>{
-	const {username, password} = request.body;
-
-	let authorized = false;
-
-	db.all("SELECT userid, password FROM users", function(err, rows){
-		for(var i=0; i<rows.length; i++){
-		    if(rows[i].userid == username && rows[i].password == password){
-				authorized = true;
-			} 
-		}
-		if(authorized){
-			console.log("establishing a session")
-			request.session.userId = username;
-			request.session.save()
-			response.redirect('/games')
-		}
-		else{
-			response.redirect('/index.html/?status=fail')
-		}
-	})
-})
-
-const checkAuth = (request, response, next) => {
-    if (request.session.userId) {
-        next(); //user has a session established, continue with next middleware
-    } else {
-        response.redirect('/index.html/?status=fail');//redirect user to the login page
-    }
-}
-
-app.get('/index.html', routes.index)
-app.get('/games', checkAuth, routes.games)
+app.post('/login', routes.login)
+app.post('/register',routes.register)
+app.get(['/','/index.html'], routes.index)
+app.get('/dashboard', checkAuth, routes.dashboard)
+app.get('/users', checkAuth, checkRole, routes.users)//checkRole implies checkAuth
 app.get('/game/*', checkAuth, routes.gameDetails)
+app.get('/searchGame', checkAuth, routes.searchGame)
+app.get('/deleteGame', checkAuth, routes.deleteGame)
 
-app.get('/searchGame', (request, response) => {//route for searching games
-	let game = (request.query.title)//.split(' ').join('+')
-	console.log(game)
-	if(!game) {
-	  response.json({message: 'Please enter a name of a game'})
-	  return
-	}
-
-	//url of the api
-	const apiPath = "https://api.igdb.com/v4/games"
-
-	//set the header for the request, required by the api
-	let headers = new Headers()
-	headers.append("Client-ID", CLIENT_ID)
-	headers.append("Authorization", `Bearer ${AUTH}`);
-	
-	//https://api-docs.igdb.com/?javascript#game
-	//body for the api request
-	let body = `fields name, cover.*; search "${game}"; limit 20;`
-
-	const options = {
-		"method": "POST",
-		"headers":headers,
-		"body":body
-	}
-
-	fetch(apiPath, options)
-	.then((response) => response.json())//recieve a response from the api
-	.then((data) => {//recieve the json data from the api
-		console.log(data)
-
-    	response.contentType('application/json').json((data))//respond to client with the json data
-		response.end()//end the request
-	})
-	.catch((error) => {
-		console.error(error)
-	})
-})
+app.get('/addGame', checkAuth, routes.addGame)
 
 app.get('/logout', (request, response) =>{
-	console.log("delete session")
-	request.session.destroy();
-	response.redirect('/index.html/?status=logout')
+	request.session.destroy(()=>{
+		console.log("delete session")
+		response.redirect('/index.html/?status=logout')
+	})
+	
 })
 
 //start server
